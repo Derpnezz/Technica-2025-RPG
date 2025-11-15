4fcd40d6dd43e72c95f95da55563afc69efa57b6
NoobLity <noobability21@gmail.com>
2025-11-15 11:29:34 -0500

    starting out

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 export default function App() {
+  const [fadeClass, setFadeClass] = useState("fade-in"); 
+  const [started, setStarted] = useState(false);
+
+
+  useEffect(() => {
+    setFadeClass("fade-in");
+  }, []);
+
+  const startLesson = () => {
+    setFadeClass("fade-out");
+    setTimeout(() => {
+      setStarted(true);
+    }, 800); 
+  };
+
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit!
-    </main>
-  )
+    <div id = 'full-screen'> 
+    <>
+      {!started && (
+        <main id="main-wrapper" className={fadeClass}>
+          <div id="top">
+            <img src="/favicon.svg" alt="Logo" width="40" />
+            <h1>Name of our app</h1>
+          </div>
+
+          <p id="description">Brief description of what this tool does</p>
+
+          <button id="start-btn" onClick={startLesson}>
+            Start your lesson
+          </button>
+        </main>
+      )} {/*  This is the main menu*/}
+
+
+
+      {started && ( 
+        <main id="main2-wrapper" className="after-start">
+
+          <h2>Your lesson has begun!</h2>
+          <div id = 'display'> 
+              
+          </div>
+        </main>
+      )} {/* after pressing start */}
+    </>
+
+    </div>
+  );
 }
